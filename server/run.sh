6c17c744e4a8c8439ebcb2f216ab8377fad69693
iseyoshitaka <ise0615@gmail.com>
2022-06-25 10:15:10 +0900

    setup server-icon

--- server/run.sh
+++ server/run.sh
@@ -2,8 +2,10 @@
 # mods copy
 cp -rp ../mods/* ./mods/
 
+# show local ip address
+ifconfig | grep "inet" | grep -v 127.0.0.1
+
 # run server
 java -version
 java -Xmx1024M -Xms1024M -jar forge-1.12.2-14.23.5.2854.jar nogui
-
 
